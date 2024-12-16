b3e768f9b891e9f38a54d942155cac6795451810
yuli.kamakura <barnett.yuxiang@gmail.com>
2024-12-16 20:02:46 +0800

    Refactor styles and update background script; remove options page and related files

--- background.js
+++ background.js
@@ -1,5 +1,11 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(data) {
-	chrome.tabs.get(data.tabId, function(tab) {
-		chrome.tabs.executeScript(data.tabId, {code: 'if (typeof AddScreenshotButton !== "undefined") { AddScreenshotButton(); }', runAt: 'document_start'});
-	});
-}, {url: [{hostSuffix: '.youtube.com'}]});
+chrome.webNavigation.onHistoryStateUpdated.addListener(
+  function (data) {
+    chrome.tabs.get(data.tabId, function (tab) {
+      chrome.tabs.executeScript(data.tabId, {
+        code: 'if (typeof AddScreenshotButton !== "undefined") { AddScreenshotButton(); }',
+        runAt: "document_start",
+      });
+    });
+  },
+  { url: [{ hostSuffix: ".youtube.com" }] }
+);
